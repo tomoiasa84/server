d38a39319b40338922993a4f4ed58e9020022fcf
tudoradrian <itudor.adrian@gmail.com>
2019-06-10 11:27:43 +0300

    knex sql raw migrations

--- knex_migrations/migrations/20190527140932_create_userx.js
+++ knex_migrations/migrations/20190527140932_create_userx.js
@@ -7,9 +7,6 @@ exports.up = function(knex, Promise) {
         t.dateTime('deletedAt').nullable();
 
         t.string('name').notNull();
-        //t.text('decription').nullable();
-        //t.decimal('price', 6, 2).notNull();
-        //t.enum('category', ['apparel', 'electronics', 'furniture']).notNull();
     });
 };
 
