021deff898b8f9f97d8992666a1df30f7250fe64
tudoradrian <itudor.adrian@gmail.com>
2019-10-08 22:32:40 +0300

    resolve checked account

---
+++ clearAllUsers.js
@@ -0,0 +1,34 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./db/adminkey");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://contractorsearch-eeaf7.firebaseio.com"
+});
+async function listAllUsers(nextPageToken) {
+  // List batch of users, 1000 at a time.
+  await admin
+    .auth()
+    .listUsers(50, nextPageToken)
+    .then(function(listUsersResult) {
+      listUsersResult.users.forEach(async function(userRecord) {
+        await admin
+          .auth()
+          .deleteUser(userRecord.uid)
+          .then(function() {
+            console.log("Successfully deleted user");
+          })
+          .catch(function(error) {
+            console.log("Error deleting user:", error);
+          });
+      });
+      if (listUsersResult.pageToken) {
+        // List next batch of users.
+        listAllUsers(listUsersResult.pageToken);
+      }
+    })
+    .catch(function(error) {
+      console.log("Error listing users:", error);
+    });
+}
+// Start listing users from the beginning, 1000 at a time.
+listAllUsers();
